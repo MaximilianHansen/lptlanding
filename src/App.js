951bd8a9ab4d1af0be7703868e00983acf568363
max <max.blade.hansen@gmail.com>
2024-04-14 10:07:03 -0400

    added features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import SignUp from "./LandingPage1"
 import SignUp1 from './LandingPage2';
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      
     <Routes>
       <Route path="/" element={<SignUp />} />
       <Route path="/signUp1" element={<SignUp1 />} />
